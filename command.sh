8958bd8a2acaa8be9aa15e721604ce2906a3ce03
Pixie Axerup <87065642+Pixie-Axerup@users.noreply.github.com>
2022-12-13 11:39:02 +0100

    Preface markdown (#12)
    * added preface markdown
    
    * added preface markdown

--- command.sh
+++ command.sh
@@ -1,3 +1,4 @@
 #!/bin/sh -l
 echo "running md2cf script"
-md2cf --host "$1" --token "$2" --parent-id "$3" --space "$4" --preface-markdown ["Content on this page is auto-generated from Github repository; do not edit here."] --strip-top-header "$5"
+md2cf --host "$1" --token "$2" --parent-id "$3" --space "$4" --preface-markdown "Content on this page is auto-generated from Github repository; do not edit here." --strip-top-header "$5"
+
