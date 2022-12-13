9bbec1e15df4e62555b9bdd2f09c12e0bca696ba
Pixie-Axerup <pixie.axerup@ingka.ikea.com>
2022-12-13 13:14:01 +0100

    added if statement

--- command.sh
+++ command.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -l
 echo "running md2cf script"
-if [ $3 = '' ]
+if [ "$3" = '' ]
 then
   md2cf --host "$1" --token "$2" --space "$4" --preface-markdown "Content on this page is auto-generated from Github repository; do not edit here." --strip-top-header "$5"
 else
